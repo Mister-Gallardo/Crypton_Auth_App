a48cc280dd715daea35c40a8e22283984c976d3b
Amir Azizov <creator005@mail.ru>
2025-02-02 17:56:01 +0300

    browser router

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./app/App.tsx";
 import { BrowserRouter } from "react-router-dom";
 
-
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter basename="/Crypton_Auth_App">
